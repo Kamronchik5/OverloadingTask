792eead50022cbd9ab60c9268aa9f9fd0d69e907
Xabibullayev Kamronbek <simplekux8082@icloud.com>
2024-10-01 23:53:45 +0500

    Added

---
+++ src/MathFunctions.java
@@ -0,0 +1,84 @@
+public class MathFunctions {
+    public int add(int a , int b){
+        return a+b;
+    }
+    public float add(float a , float b){
+        return a+b;
+    }
+    public int add(byte a , byte b){
+        return a+b;
+    }
+    public int add(short a , short b){
+        return  a+b;
+    }
+
+    public int sub(int a , int b){
+        return a-b;
+    }
+    public float sub(float a , float b){
+        return a-b;
+    }
+    public int sub(byte a , byte b){
+        return a-b;
+    }
+    public int sub(short a , short b){
+        return  a-b;
+    }
+
+    public int multiply(int a , int b){
+        return a*b;
+    }
+    public float multiply(float a , float b){
+        return a*b;
+    }
+    public int multiply(byte a , byte b){
+        return a*b;
+    }
+    public int multiply(short a , short b){
+        return  a*b;
+    }
+
+    public int div(int a , int b){
+        return a/b;
+    }
+    public float div(float a , float b){
+        return a/b;
+    }
+    public int div(byte a , byte b){
+        return a/b;
+    }
+    public int div(short a , short b){
+        return  a/b;
+    }
+
+    public int abs(int a , int b){
+        int x = a-b;
+        return Math.abs(x);
+    }
+    public float abs(float a , float b){
+        float x = a-b;
+        return Math.abs(x);
+    }
+    public int abs(byte a , byte b){
+        int x = a-b;
+        return Math.abs(x);
+    }
+    public int abs(short a , short b){
+        int x = a-b;
+        return Math.abs(x);
+    }
+
+    public int pow(int a){
+        return a*a;
+    }
+    public float pow(float a){
+        return a*a;
+    }
+    public int pow(byte a){
+        return a*a;
+    }
+    public int pow(short a){
+        return  a*a;
+    }
+
+}
